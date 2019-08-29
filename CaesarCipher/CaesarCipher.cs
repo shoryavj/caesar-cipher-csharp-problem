b5ba062eea453ba47f4454fecabcc30947926c35
shorya.vijayvargiya <shorya.vijayvargiya@cgi.com>
2019-08-29 17:37:38 +0530

    Hi

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -1,16 +1,48 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace CaesarCipher
 {
     public static class RotationalCipher
     {
         public static string Rotate(string text, int shiftKey)
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+            {
+                        
+                        string str= ""; 
+
+                        for (int i=0; i<text.Length; i++) 
+                        { 
+                            if(char.IsLetter(text[i]))
+                            {
+                                if (char.IsUpper(text[i]))
+                                { 
+                                    char ch = (char)((text[i] + shiftKey - 65) % 26 + 65); 
+                                    str += ch; 
+                                } 
+                                else
+                                { 
+                                    char ch = (char)((text[i] + shiftKey - 97) % 26 + 97); 
+                                    str += ch; 
+                                } 
+
+                             }
+                            else{
+                                    str=str+text[i];
+                                }
+                        } 
+                        return str; 
+            } 
+
         public static void Main(string[] args){
             
-        }
-    }
-}
+                                             }
+    }  
+                
+       
+    
+}  
+
+        
+     
+
